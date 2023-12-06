e07d4978cc14a13427b7161469f68b5fdb0044c2
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-06 19:39:27 +0530

    4.1

---
+++ experiment4/4.2.py
@@ -0,0 +1,6 @@
+topic=str(input("Enter topic"))
+
+if(topic=='A') or (topic=='B') or (topic=='C') or (topic=='a') or (topic=='b') or (topic=='c'):
+    print("there is a possibility for Anil to win")
+else:
+    print("Anil will fail")
