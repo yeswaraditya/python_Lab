ca5774704695768cf01f0215fde436e8d8bc68ad
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-06 06:26:10 +0530

    2.2 cde

---
+++ Experiment2/2.2.py
@@ -0,0 +1,11 @@
+print("enter diameter")
+diameter=float(input())
+radius=diameter/2
+area=3.14*radius*radius
+perimeter=2*3.14*radius
+print("radius is ")
+print(radius)
+print("area is")
+print(area)
+print("perimeter is")
+print(perimeter)
