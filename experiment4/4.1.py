ddd6220babd90ceaf74d5e2421e69fcecff28ba8
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-06 07:06:37 +0530

    4.1

---
+++ experiment4/4.1.py
@@ -0,0 +1,24 @@
+name=str(input("enter student name"))
+student_class=str(input("enter student class"))
+section=str(input("enter student section"))
+sub1=int(input("enter maths marks"))
+sub2=int(input("enter physics marks"))
+sub3=int(input("enter social marks"))
+sub4=int(input("enter biology marks"))
+sub5=int(input("enter hindi marks"))
+totalmarks=sub1+sub2+sub3+sub4+sub5
+percentage=(totalmarks/5)
+print("Name : ",name) 
+print("Class : ",student_class) 
+print("Section : ",section)
+print("percentage :",percentage)
+
+if percentage < 45:
+    print("status:fail")
+    
+elif percentage <:
+    print("status:fail")
+if percentage < 45:
+    print("status:fail")
+if percentage < 45:
+    print("status:fail")
