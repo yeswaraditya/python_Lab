87c4d7a4b4016a56bb50c7d61ce909b47f504b6f
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-06 06:40:18 +0530

    3.2

---
+++ Experment3/3.2.py
@@ -0,0 +1,13 @@
+color=str(input(""))
+if (color=="red" or color == "Red") :
+    print("car is has to stop")
+    
+elif (color=="yellow" or "Yellow"):
+    print("car has to wait")
+    
+elif (color=="green" or "Green"):
+    print("car has to go")
+    
+else :
+    print("unrecognised color")
+    
