5210128ee0de11089d7a96dfb4201efd291e432f
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-06 20:28:21 +0530

    5.5

---
+++ Experiment5/5.5.py
@@ -0,0 +1,11 @@
+# pattern
+
+for i in range(1,5+1):
+    for j in range (1,i+1):
+        print("*",end=" ")
+    print()
+    
+for i in range(4,0,-1):
+    for j in range(0,i):
+        print("*",end=" ")
+    print()
