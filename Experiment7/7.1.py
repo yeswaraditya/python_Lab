8e3a103be298622159c95ea0c5206394c18d5a1d
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-07 06:26:27 +0530

    7.1

---
+++ Experiment7/7.1.py
@@ -0,0 +1,8 @@
+# Write a python program to Reverse a given string
+
+str1="hello"
+print("original string",str1)
+# Reversing the string using reversed() and joining the characters
+str1="".join(reversed(str1))
+
+print("reversed string is:",str1)
